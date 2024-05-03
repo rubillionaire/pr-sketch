7d5cb78e129b97a2a1c836d6cafba28b15646b84
rubonics <rubenrodriguez+github@gmail.com>
2024-05-03 13:13:47 -0400

    [mie] -00,-01
    - 00
    - terrain over blue bg
    - 01
    - adds ocean shader and taper into ocean

---
+++ mie-00/index.js
@@ -0,0 +1,240 @@
+// mie-00
+// - terrain shader over water
+const mixmap = require('@rubenrodriguez/mixmap')
+const regl = require('regl')
+const resl = require('resl')
+const { default: MixmapPMTiles, TileType, RasterShaders } = require('mixmap-pmtiles')
+const { default: tileGrid } = require('mixmap-tile-grid')
+const mix = mixmap(regl, {
+  extensions: ['oes_element_index_uint'],
+  attributes: {
+    stencil: true,
+  },
+})
+
+const colorRgb = {
+  green: [97, 192, 113],
+  blue: [26, 114, 187],
+  white: [255, 255, 255],
+}
+const colorGlsl = {}
+for (const color in colorRgb) {
+  colorGlsl[color] = colorRgb[color].map(c => c/255)
+}
+
+function prViewbox () {
+  const prWE = [-67.356661, -65.575714] 
+  const prCenter = 18.220148006000038
+  // screen height/width = prHeight/prWidth
+  // screen height/width  * prWidth = prHeight
+  const prHorizontal = (prWE[1] - prWE[0])
+  const prHeight = (window.innerHeight/window.innerWidth * prHorizontal)
+  const prSN = [prCenter - prHorizontal/2, prCenter + prHorizontal/2]
+  const viewbox = [prWE[0],prSN[0],prWE[1],prSN[1]]
+  return viewbox
+}
+
+const map = mix.create({
+  viewbox: prViewbox(),
+  backgroundColor: [0.5, 0.5, 0.5, 1.0],
+  clear: {
+    color: [0.5, 0.5, 0.5, 1.0],
+    depth: 1,
+    stencil: 0,
+  },
+})
+
+function addElevation ({ bgZindex, elevationZindex }) {
+  const { raster } = RasterShaders(map)
+  const bg = {
+    ...raster,
+    uniforms: {
+      ...raster.uniforms,
+      zindex: bgZindex,
+      colorBg: colorGlsl.white,
+    },
+    frag: `
+      precision highp float;
+      uniform vec3 colorBg;
+      uniform sampler2D texture;
+      varying vec2 vtcoord;
+      void main() {
+        vec4 sample = texture2D(texture, vtcoord);
+        gl_FragColor = vec4(colorBg, sample.a);
+      }
+    `
+  }
+  const elevation = {
+    ...raster,
+    uniforms: {
+      ...raster.uniforms,
+      maxElevation: 1016.1,
+      dotColor: colorGlsl.green,
+      backgroundColor: colorGlsl.white,
+      zindex: elevationZindex,
+    },
+    vert: `
+      precision highp float;
+
+      attribute vec2 position;
+      attribute vec2 tcoord;
+      uniform vec4 viewbox;
+      uniform vec2 offset;
+      uniform float aspect, zindex;
+      varying vec2 vtcoord;
+      varying vec2 vpos;
+
+      void main () {
+        vec2 p = position + offset;
+        vtcoord = tcoord;
+        vpos = position;
+        gl_Position = vec4(
+          (p.x - viewbox.x) / (viewbox.z - viewbox.x) * 2.0 - 1.0,
+          ((p.y - viewbox.y) / (viewbox.w - viewbox.y) * 2.0 - 1.0) * aspect,
+          1.0/(1.0 + zindex),
+          1.0
+        );
+      }
+    `,
+    frag: `
+      precision highp float;
+
+      uniform sampler2D texture;
+      uniform float maxElevation, zoom;
+      uniform vec2 texelSize;
+      uniform vec3 dotColor;
+      uniform vec3 backgroundColor;
+
+      varying vec2 vtcoord;
+      varying vec2 vpos;
+
+      const vec2 zoomRange = vec2(8., 12.);
+
+      float circle ( vec2 _st, float _radius ) {
+        vec2 pos = vec2( 0.5 ) - _st;
+        return smoothstep( 1.0 - _radius, 1.0 - _radius + _radius * 0.2, 1.-dot( pos, pos ) * 3.14 );
+      }
+
+      vec2 tileSpace ( in vec2 _st, in vec2 numberOfTiles ) {
+        _st *= numberOfTiles;
+        _st = fract( _st );
+        return _st;
+      }
+
+      vec2 tileSpaceCenter(in vec2 _st, in vec2 numberOfTiles) {
+        _st *= numberOfTiles;
+        return (floor(_st) + 0.5) / numberOfTiles;
+      }
+
+      float texelToElevation (vec3 texel) {
+        return -10000.0 + ((texel.r * 256.0 * 256.0 * 256.0 + texel.g * 256.0 * 256.0 + texel.b * 256.0) * 0.1);
+      }
+
+      void main () {
+        vec4 sample = texture2D(texture, vtcoord);
+        if (sample.a < 1.0) {
+          gl_FragColor = vec4(0.0);
+          return;
+        }
+        float z = smoothstep(zoomRange.x, zoomRange.y, zoom);
+        float tileDim = mix(80.0, 700.0, z);
+        vec2 tileSize = vec2(tileDim);
+        vec2 origin = tileSpaceCenter(vtcoord, tileSize);
+        vec4 originSample = texture2D(texture, origin);
+        vec2 tiled = tileSpace(vpos, tileSize);
+        float elevation = texelToElevation(originSample.xyz);
+        float normElevation = clamp(elevation/maxElevation, 0.0, 1.0);
+        float inCircle = circle(
+          tiled,
+          mix(0.1, 0.7, normElevation)
+        );
+        float opacity = mix(0., 1., inCircle);
+        vec3 color = mix(backgroundColor, dotColor, inCircle);
+        gl_FragColor = vec4(color, opacity);
+      }
+    `,
+  }
+  new MixmapPMTiles(map, {
+    source: 'https://rr-studio-assets.nyc3.digitaloceanspaces.com/pr-sketch/mie-prefecture/terrain-dem-v1-clipped-pr.pmtiles',
+    tileType: TileType.Png,
+    shaders: {
+      elevation,
+    },
+  })
+}
+
+function addWaterBg ({ zindex }) {
+  const { positions, cells } = fullEarthMesh()
+  const draw = map.createDraw({
+    attributes: {
+      position: positions,
+    },
+    elements: cells,
+    uniforms: {
+      zindex,
+      colorBg: colorGlsl.white,
+      colorFg: colorGlsl.blue,
+    },
+    frag: `
+      precision highp float;
+      uniform vec3 colorBg, colorFg;
+      varying vec2 vpos;
+      void main () {
+        float isFg = mod(vpos.y, 0.001);
+        vec3 color = mix(
+          colorFg,
+          colorBg,
+          step(isFg, 0.0004)
+        );
+        gl_FragColor = vec4(colorFg, 1.0);
+      }
+    `,
+    blend: {
+      enable: true,
+      func: { src: 'src alpha', dst: 'one minus src alpha' },
+    },
+  })
+  draw.props.push({})
+}
+
+
+// tileGrid(map, { zindex: 1 })
+addWaterBg({ zindex: 1 })
+addElevation({ bgZindex: 2, elevationZindex: 3 })
+
+window.addEventListener('keydown', function (ev) {
+  if (ev.code === 'Equal') {
+    map.setZoom(Math.min(20,Math.round(map.getZoom()+1)))
+  } else if (ev.code === 'Minus') {
+    map.setZoom(map.getZoom()-1)
+  }
+  console.log('zoom', map.getZoom())
+})
+window.addEventListener('resize', () => {
+  map.resize(window.innerWidth, window.innerHeight)
+})
+document.body.style.margin = '0'
+document.body.appendChild(mix.render())
+document.body.appendChild(map.render({
+  width: window.innerWidth,
+  height: window.innerHeight,
+}))
+
+function bboxToMesh (bbox) {
+  return {
+    positions: [
+      bbox[0], bbox[1],
+      bbox[0], bbox[3],
+      bbox[2], bbox[3],
+      bbox[0], bbox[1],
+      bbox[2], bbox[3],
+      bbox[2], bbox[1]
+    ],
+    cells: [0, 1, 2, 3, 4, 5]
+  }
+}
+
+function fullEarthMesh () {
+  const bbox = [-180, -90, 180, 90]
+  return bboxToMesh(bbox)
+}

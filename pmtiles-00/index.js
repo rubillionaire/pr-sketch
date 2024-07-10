ae3f6e3c6734b94370fbcfbff5b3fccd41ff6b41
rubonics <rubenrodriguez+github@gmail.com>
2024-07-10 18:08:58 -0400

    [pmtiles] -00
    + actively being used to debug mixmap-pmtiles

--- pmtiles-00/index.js
+++ pmtiles-00/index.js
@@ -3,10 +3,8 @@
 const mixmap = require('@rubenrodriguez/mixmap')
 const regl = require('regl')
 const resl = require('resl')
-const geojson2mesh = require('earth-mesh')
-const { default: MixmapPMTiles } = require('mixmap-pmtiles')
+const { default: MixmapPMTiles, TileType, RasterShaders } = require('mixmap-pmtiles')
 const { default: tileGrid } = require('mixmap-tile-grid')
-
 const mix = mixmap(regl, {
   extensions: ['oes_element_index_uint'],
   attributes: {
@@ -14,18 +12,30 @@ const mix = mixmap(regl, {
   },
 })
 
-const prWE = [-67.356661, -65.575714] 
-const prCenter = 18.220148006000038
-// screen height/width = prHeight/prWidth
-// screen height/width  * prWidth = prHeight
-const prHorizontal = (prWE[1] - prWE[0])
-const prHeight = (window.innerHeight/window.innerWidth * prHorizontal)
-const prSN = [prCenter - prHorizontal/2, prCenter + prHorizontal/2]
-const viewbox = [prWE[0],prSN[0],prWE[1],prSN[1]]
-console.log({viewbox})
+const colorRgb = {
+  green: [97, 192, 113],
+  blue: [26, 114, 187],
+  white: [255, 255, 255],
+}
+const colorGlsl = {}
+for (const color in colorRgb) {
+  colorGlsl[color] = colorRgb[color].map(c => c/255)
+}
+
+function prViewbox () {
+  const prWE = [-67.356661, -65.575714] 
+  const prCenter = 18.220148006000038
+  // screen height/width = prHeight/prWidth
+  // screen height/width  * prWidth = prHeight
+  const prHorizontal = (prWE[1] - prWE[0])
+  const prHeight = (window.innerHeight/window.innerWidth * prHorizontal)
+  const prSN = [prCenter - prHorizontal/2, prCenter + prHorizontal/2]
+  const viewbox = [prWE[0],prSN[0],prWE[1],prSN[1]]
+  return viewbox
+}
 
 const map = mix.create({
-  viewbox,
+  viewbox: prViewbox(),
   backgroundColor: [0.5, 0.5, 0.5, 1.0],
   clear: {
     color: [0.5, 0.5, 0.5, 1.0],
@@ -38,72 +48,20 @@ tileGrid(map, { zindex: 1 })
 
 var style = new Image
 style.onload = function () {
-  const tiles = new MixmapPMTiles(map, {
+  new MixmapPMTiles(map, {
+    // source: 'https://rr-studio-assets.nyc3.digitaloceanspaces.com/pr-sketch/pr.osm-z20.pmtiles',
     source: 'https://rr-studio-assets.nyc3.digitaloceanspaces.com/pr-sketch/pr.osm.pmtiles',
     // source: 'https://rr-studio-assets.nyc3.digitaloceanspaces.com/pr-sketch/pr-tile-simplified.pmtiles',
+    // source: './pmtiles/stamen_toner(raster)CC-BY+ODbL_z3.pmtiles',
+    // source: './pmtiles/terrain-dem-Z8-z12.pmtiles',
+    // source: './pmtiles/terrain-dem-v1-clipped-pr.pmtiles',
+    // source: './pmtiles/tmp-dem-clipped.pmtiles',
+    tileType: TileType.Mvt,
     style,
-    filterFeature: (feature) => {
-      const isIsland = feature.properties?.place === 'island'
-      return isIsland
-    },
   })
 }
-// style.src = './style-textures/isolate-place-island.png'
-style.src = './style-textures/flaneur-yuv.png'
-
-
-// const drawNE = map.createDraw({
-//   vert: `
-//     precision highp float;
-
-//     attribute vec2 position;
-    
-//     uniform vec4 viewbox;
-//     uniform vec2 offset;
-//     uniform float aspect, zindex;
-
-//     void main () {
-//       vec2 p = position.xy + offset;
-//       gl_Position = vec4(
-//         (p.x - viewbox.x) / (viewbox.z - viewbox.x) * 2.0 - 1.0,
-//         ((p.y - viewbox.y) / (viewbox.w - viewbox.y) * 2.0 - 1.0) * aspect,
-//         zindex, 1);
-//     }
-//   `,
-//   frag: `
-//     precision highp float;
-
-//     void main () {
-//       gl_FragColor = vec4(1,0,0,1);
-//     }
-//   `,
-//   attributes: {
-//     position: map.prop('positions'),
-//   },
-//   uniforms: {
-//     aspect: ({ viewportWidth, viewportHeight }) => viewportWidth/viewportHeight,
-//     zindex: -2, // put it below everything else, toggle up to see it over the pmtiles data
-//   },
-//   elements: map.prop('cells'),
-// })
-
-resl({
-  manifest: {
-    neGeojson: {
-      type: 'text',
-      src: 'ne-10m-land-pr.json',
-      parser: JSON.parse,
-    },
-  },
-  onDone: ({ neGeojson }) => {
-    const neMesh = geojson2mesh(neGeojson)
-    // drawNE.props.push({
-    //   positions: neMesh.triangle.positions,
-    //   cells: neMesh.triangle.cells,
-    // })
-    // map.draw()
-  },
-})
+style.src = './style-textures/isolate-place-island.png'
+// style.src = './style-textures/flaneur-yuv.png'
 
 window.addEventListener('keydown', function (ev) {
   if (ev.code === 'Equal') {
@@ -122,3 +80,22 @@ document.body.appendChild(map.render({
   width: window.innerWidth,
   height: window.innerHeight,
 }))
+
+function bboxToMesh (bbox) {
+  return {
+    positions: [
+      bbox[0], bbox[1],
+      bbox[0], bbox[3],
+      bbox[2], bbox[3],
+      bbox[0], bbox[1],
+      bbox[2], bbox[3],
+      bbox[2], bbox[1]
+    ],
+    cells: [0, 1, 2, 3, 4, 5]
+  }
+}
+
+function fullEarthMesh () {
+  const bbox = [-180, -90, 180, 90]
+  return bboxToMesh(bbox)
+}
